4524043255d89c623a554759b999d1be251c0b5c
frmazzoletti <franco_mazzoletti@hotmail.com>
2020-05-02 19:10:28 -0300

    Creates badic html layout on index.html, links to script.js. Implements full rock paper scissors console game

---
+++ script.js
@@ -0,0 +1,64 @@
+const selections = ["rock", "paper", "scissors"];
+
+const score = [0, 0]; //player score at [0], computer score at[1]
+
+function computerPlay(){
+    
+    return selections[Math.floor( Math.random() * 3)];
+}
+
+function playRound(player, computer){
+
+    let playerWin;
+    let result;
+    let playerId = selections.indexOf(player);
+    let computerId = selections.indexOf(computer);
+
+    let capSelections = ["Rock", "Paper", "Scissors"];
+
+    if (playerId === computerId){
+
+        result = `Tie! ${capSelections[playerId]} ties to ${selections[computerId]}.`;
+
+    } else if (playerId == 0 && computerId == 2 || playerId == 1 && computerId == 0 || playerId == 2 && computerId == 1){
+
+        result = `You win! ${capSelections[playerId]} beats ${selections[computerId]}.`;
+        score[0]++;
+
+    } else {
+
+        result = `You lose! ${capSelections[computerId]} beats ${selections[playerId]}.`;
+        score[1]++;
+
+    }
+
+    return result;
+}
+
+
+function game(){
+
+    for (let i = 0; i < 5; i++){
+
+        const computerSelection = computerPlay();
+        let playerSelection;
+
+        while (!selections.includes(playerSelection)){
+
+            playerSelection = prompt("Pick rock, paper or scissors.").toLowerCase();
+        }
+
+        console.log(`Round ${i+1}: ${playRound(playerSelection, computerSelection)}`);
+
+    }
+
+    if (score[0] > score[1]){
+        console.log(`You win! ${score[0]} to ${score[1]}.`);
+    } else if (score[0] < score[1]){
+        console.log(`Computer wins! ${score[1]} to ${score[0]}.`);
+    } else {
+        console.log(`Tie! ${score[0]} to ${score[1]}.`)
+    }
+}
+
+game();
